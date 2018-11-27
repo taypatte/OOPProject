8c35c622bb34c9a279dfc53da1f9ea8a5ad114a7
glengogo <32129936+xx345172@users.noreply.github.com>
2018-11-26 21:28:16 -0600

    Update CheckingAccount.java

--- CheckingAccount.java
+++ CheckingAccount.java
@@ -1,13 +1,11 @@
-
-
 public class CheckingAccount extends Account {
 
     public CheckingAccount(String accountNumber, String pinNumber, String ssn, long balance) {
-        super(accountNumber, pinNumber, ssn, balance);
+        super(accountNumber, pinNumber, ssn, balance,"checking");
     }
 
     public CheckingAccount(String accountNumber, String pinNumber, String ssn) {
-        super(accountNumber, pinNumber, ssn);
+        super(accountNumber, pinNumber, ssn,"checking");
     }
     public void viewLastDeposit() {
         System.out.println("Viewing Last Deposit");
